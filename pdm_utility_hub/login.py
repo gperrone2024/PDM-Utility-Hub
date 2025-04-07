e3bcd8009c5d9c28e5e36486f18090ad5ed33219
gperrone2024 <gabriele.perrone@redcare-pharmacy.com>
2025-04-07 20:17:20 +0200

    Update login.py

--- pdm_utility_hub/login.py
+++ pdm_utility_hub/login.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Login - PDM Utility Hub", page_icon="🔑")
 # Se l'utente è già loggato, mostra un messaggio e il link per andare all'HUB
 if st.session_state.get("password_correct", False):
     st.success("Sei già loggato!")
-    st.markdown("[Vai all'HUB Principale](./pdm_utility_hub/pages/pdm_hub.py)")
+    st.markdown("[Vai all'HUB Principale](./pages/pdm_hub.py)")
     st.stop()
 
 st.title("Login senza Password")
