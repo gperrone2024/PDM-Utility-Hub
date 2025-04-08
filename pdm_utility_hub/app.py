f27e9c2c5e852da2bab22b1718bf92b94e37aca5
gperrone2024 <gabriele.perrone@redcare-pharmacy.com>
2025-04-08 15:27:37 +0200

    Update app.py

--- pdm_utility_hub/app.py
+++ pdm_utility_hub/app.py
@@ -79,23 +79,14 @@ st.markdown("**Welcome to the Product Data Management Utility Hub. Select an app
 col1, col2 = st.columns(2)
 
 with col1:
-   
-      st.markdown("""
+    st.markdown("""
     <div class="app-container">
-        <a href="/?page=Bundle_Set_Images_Creator" target="_self" class="app-button-link">
+        <a href="/Bundle_Set_Images_Creator" target="_self" class="app-button-link">
             📦 Bundle & Set Images Creator
         </a>
         <p class="app-description">
             Automatically downloads, processes, and organizes images for product bundles and sets.
         </p>
-    </div>
-    """, unsafe_allow_html=True)
-
-    st.markdown("""
-    <div class="app-container">
-        <div class="app-button-placeholder">
-            🚧 Coming Soon
-        </div>
     </div>
     """, unsafe_allow_html=True)
 
